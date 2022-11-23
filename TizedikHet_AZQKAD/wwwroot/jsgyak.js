dc178f7a73fd3b23b69b8d4df7b59c9c83c41744
azqkad <azqkad@SZA0030900>
2022-11-23 17:17:16 +0100

    sok munka kell

--- TizedikHet_AZQKAD/wwwroot/jsgyak.js
+++ TizedikHet_AZQKAD/wwwroot/jsgyak.js
@@ -1,1 +1,20 @@
-﻿
+﻿for (var sor = 0; sor < 10; sor++) {
+    //új div létrehozása az új sornak
+    //új div osztálylistájához add hozzá a "sor"-t
+    //új div-et add hozzá a "pascal" gyermekeihez
+    for (var oszlop = 0; oszlop <= sor; oszlop++) {
+        //új div létrehozása az új elemnek
+        //új elem div osztálylistájához add hozzá az "elem"-et
+        //teszteléshet .innerHTML = `${sor}:${oszlop}`
+        //legyen az innerHTML a megfelelő szám
+        //új elem div-et vedd fel a sor elemei közé
+    }
+}
+
+var faktoriális = function (n) {
+    let er = 1;
+    for (let i = 2; i <= n; i++) {
+        er = er * i;
+    }
+    return er;
+}
