ae6ad77b01153459a01ae6381c83c79995a7a9e2
roland8994 <roland.hidasi@stud.uni-corvinus.hu>
2022-12-09 18:30:06 +0100

    kerdes

---
+++ TizedikHet_AZQKAD/Controllers/BoatController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TizedikHet_AZQKAD.Models;
+
+namespace TizedikHet_AZQKAD.Controllers
+{
+    //[Route("api/[controller]")]
+    [ApiController]
+    public class BoatController : ControllerBase
+    {
+        [HttpGet]
+        [Route("questions/all")]
+        public IActionResult MindegyHogyHivjak()
+        {
+            HajosContext context = new HajosContext();
+            var kérdések = from x in context.Questions select x.Question1;
+
+            return Ok(kérdések);
+        }
+    }
+}
---
+++ TizedikHet_AZQKAD/Controllers/TesztController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TizedikHet_AZQKAD.Controllers
+{
+    public class TesztController : Controller
+    {
+        [HttpGet]
+        [Route("corvinus/szerverido")]
+        public IActionResult M1()
+        {
+            string pontosIdő = DateTime.Now.ToShortTimeString();
+
+            return new ContentResult
+            {
+                ContentType = System.Net.Mime.MediaTypeNames.Text.Plain, //"text/plain"
+                Content = pontosIdő
+            };
+        }
+    }
+}
